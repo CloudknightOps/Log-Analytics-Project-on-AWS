85432e9f9799d9725291ecacdd1b3438ad0b373c
Theooo <Uviekugbere.theo@gmail.com>
2023-02-04 23:31:20 +0100

    Created the Fake-apache-log-generator

---
+++ Fake-Log-generator/apache-fake-log-gen.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+import time
+import datetime
+import numpy
+import random
+import gzip
+import zipfile
+import sys
+import argparse
+from faker import Faker
+from random import randrange
+from tzlocal import get_localzone
+
+local = get_localzone()
+
+class Switch:
+    def __init__(self, value):
+        self.value = value
+        self.fall = False
+
+    def __iter__(self):
+        yield self.match
+        raise StopIteration
+
+    def match(self, *args):
+        if self.fall or not args:
+            return True
+        elif self.value in args:
+            self.fall = True
+            return True
+        else:
+            return False
+
+parser = argparse.ArgumentParser(description="Fake Apache Log Generator")
+parser.add_argument("--output", "-o", dest='output_type', help="Write to a Log file, a gzip file or to STDOUT", choices=['LOG','GZ','CONSOLE'] )
+parser.add_argument("--log-format", "-l", dest='log_format', help="Log format, Common or Extended Log Format ", choices=['CLF','ELF'], default="ELF" )
+parser.add_argument("--num", "-n", dest='num_lines', help="Number of lines to generate (0 for infinite)", type=int, default=1)
+parser.add_argument("--prefix", "-p", dest='file_prefix', help="Prefix the output file name", type=str)
+parser.add_argument("--sleep", "-s", help="Sleep this long between lines (in seconds)", default=0.0, type=float)
+
+args = parser.parse_args()
+
+log_lines = args.num_lines
+file_prefix = args.file_prefix
+output_type = args.output_type
+log_format = args.log_format
+
+faker = Faker()
+
+timestr = time.strftime("%Y%m%d-%H%M%S")
+otime = datetime.datetime.now()
+
+outFileName = 'access_log_'+timestr+'.log' if not file_prefix else file_prefix+'_access_log_'+timestr+'.log'
+
+for case in Switch(output_type):
+    if case('LOG'):
+        f = open(outFileName,'w')
+        break
+    if case('GZ'):
+        f = gzip.open(outFileName+'.gz','wb')
+        break
+    if case('CONSOLE'): pass
+    if case():
+        f = sys.stdout
+
+response=["200","404","500","301"]
+
+verb=["GET","POST","DELETE","PUT"]
+
+resources=["/list","/wp-content","/wp-admin","/explore","/search/tag/list","/app/main/posts","/posts/posts/explore","/apps/cart.jsp?appID="]
+
+ualist = [faker.firefox, faker.chrome, faker.safari, faker.internet_explorer, faker.opera]
+
+flag = True
