a49d840485bed0e3bdc52793e393a45d9b257c2b
Theoooo <Uviekugbere.theo@gmail.com>
2023-03-10 23:43:34 +0100

    Corrected the cloudformation (network.yml) script)

--- Cloudformation-Template/Compute.sh
+++ Cloudformation-Template/Compute.sh
@@ -3,5 +3,8 @@
 aws cloudformation create-stack \
 --stack-name Compute \
 --template-body file://Compute.yml \
+--parameters file://Compute-parameters.json
 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" \
 --region= us-east-1
+
+
